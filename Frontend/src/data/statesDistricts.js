9f6e6d9d73062e640452119886992be58ea76e1f
Prajwal M <prajwalm7781@gmail.com>
2026-06-10 22:08:06 +0530

    Added irrigation route and state district data

---
+++ Frontend/src/data/statesDistricts.js
@@ -0,0 +1,68 @@
+const statesDistricts = {
+  Karnataka: [
+    "Bagalkot",
+    "Ballari",
+    "Belagavi",
+    "Bengaluru Rural",
+    "Bengaluru Urban",
+    "Bidar",
+    "Chamarajanagar",
+    "Chikkaballapur",
+    "Chikkamagaluru",
+    "Chitradurga",
+    "Dakshina Kannada",
+    "Davanagere",
+    "Dharwad",
+    "Gadag",
+    "Hassan",
+    "Haveri",
+    "Kalaburagi",
+    "Kodagu",
+    "Kolar",
+    "Koppal",
+    "Mandya",
+    "Mysuru",
+    "Raichur",
+    "Ramanagara",
+    "Shivamogga",
+    "Tumakuru",
+    "Udupi",
+    "Uttara Kannada",
+    "Vijayapura",
+    "Yadgir"
+  ],
+
+  Maharashtra: [
+    "Mumbai",
+    "Pune",
+    "Nagpur",
+    "Nashik",
+    "Aurangabad",
+    "Kolhapur",
+    "Solapur"
+  ],
+
+  TamilNadu: [
+    "Chennai",
+    "Coimbatore",
+    "Madurai",
+    "Salem",
+    "Tiruchirappalli"
+  ],
+
+  Kerala: [
+    "Thiruvananthapuram",
+    "Kochi",
+    "Kozhikode",
+    "Kannur"
+  ],
+
+  AndhraPradesh: [
+    "Visakhapatnam",
+    "Vijayawada",
+    "Guntur",
+    "Tirupati"
+  ]
+};
+
+export default statesDistricts;
